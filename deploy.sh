75d73a2d5f026cbb05ea60dbcaece4e0b117c5b6
1996karthick <61176771+1996karthick@users.noreply.github.com>
2020-04-22 13:59:50 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,7 @@ then
     sudo su -s "$0"
     exit
 fi
-sudo echo "CREATE DATABASE zippyops_db CHARACTER SET utf8 COLLATE utf8_general_ci;;" | mysql
-sudo echo "CREATE USER 'zippyops'@'localhost' IDENTIFIED BY 'zippyops';" | mysql
-sudo echo "GRANT ALL PRIVILEGES ON zippyops_db.* TO 'zippyops'@'localhost';" | mysql
-sudo echo "FLUSH PRIVILEGES;" | mysql
+
 cd /home/ec2-user/drupal
 mysql -u zippyops -pzippyops zippyops_db < zippyops_db.sql
 yes | cp -Rf drupal /var/www/html/
